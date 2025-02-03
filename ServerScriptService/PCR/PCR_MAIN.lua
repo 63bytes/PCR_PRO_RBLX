9457183f95b5669c06b0f1e7308c2374eaf5b111
63btyes <153458135+63bytes@users.noreply.github.com>
2025-02-03 08:08:40 +0000

    Update PCR_MAIN.lua

--- ServerScriptService/PCR/PCR_MAIN.lua
+++ ServerScriptService/PCR/PCR_MAIN.lua
@@ -19,6 +19,8 @@ local PI_INITIATION_TEMP = 150
 local PI_INITIATION_TEMP_RANGE = 5
 local PI_FUEL_TEMP = 19
 local CELL_PLASMA_DIVIDER = 5
+local CP_COOL_TO = CHAMBER_AMBIENT
+local CP_POWER = 0.6
 --RUNNING DATA
 module.piTemp = {}
 module.combustionTemp = {}
@@ -27,6 +29,7 @@ module.piFuelInput = {0}
 module.cellPD = {}--Cell plasma density
 module.cellTemp = {}
 module.plasmaOutput = {}
+module.coolingPump = {}
 --vars
 local temp
 local item
@@ -63,6 +66,9 @@ end
 function cap(num, cap)
 	if num>cap then
 		num = cap
+	end
+	if num<0 then
+		num = 0
 	end
 	return num
 end
@@ -102,16 +108,22 @@ function updatePlamaOutput()
 end
 function updateCellPlasma()
 	for x=1,4 do
+		--cell temp
+		
+		--cell pd
 		module.cellPD[x] = module.cellPD[x] + module.plasmaOutput[x]/CELL_PLASMA_DIVIDER
-		module.cellTemp[x] = cap(module.cellTemp[x]+(module.combustionTemp[x]*PLASMA_TO_CELL_DISIPATION), module.combustionTemp[x])
-		temp = PLASMA_IDEAL_DISAPATION_TEMP-module.cellTemp[x]
-		if temp>=0 then
-			temp = PLASMA_DISIPATION_RATE*temp
+		module.cellTemp[x] = cap(module.cellTemp[x]+(module.combustionTemp[x]*PLASMA_TO_CELL_DISIPATION), PLASMA_REACTION_TEMP)
+		temp = module.cellTemp[x]
+		if (0<temp) and (temp<PLASMA_IDEAL_DISAPATION_TEMP) then
+			module.cellPD[x] -= (PLASMA_IDEAL_DISAPATION_TEMP-temp)*PLASMA_DISIPATION_RATE
+		elseif (PLASMA_IDEAL_DISAPATION_TEMP<temp) and (temp<PLASMA_CRITICAL_TEMP) then
+			module.cellPD[x] -= PLASMA_DISIPATION_RATE
+		elseif PLASMA_CRITICAL_TEMP<temp then
+			--stop else from executing
+		else
+			module.cellPD[x] -= PLASMA_DISIPATION_RATE
 		end
-		if temp>0-PLASMA_CRITICAL_TEMP then
-			module.cellPD[x] = module.cellPD[x] - PLASMA_DISIPATION_RATE
-		end
-		module.cellPD[x] = cap(module.cellPD[x], 0)
+		module.cellPD[x] = cap(module.cellPD[x], 1)
 	end
 end
 function piUpdate()
@@ -124,6 +136,7 @@ function cellUpdate()
 end
 local d = script.Parent.data
 local pi = d.PI
+local c = d.Cells
 function exportData()
 	for x=1,4 do
 		pi[x].temp.Value = module.piTemp[x][1]
@@ -131,6 +144,8 @@ function exportData()
 		pi[x].combustionTemp.Value = module.combustionTemp[x]
 		pi[x].plasmaOutput.Value = module.plasmaOutput[x]
 	end
+	c["1"].Temp.Value = module.cellTemp[1]
+	c["1"].PD.Value = module.cellPD[1]
 end
 
 module.update = function()
